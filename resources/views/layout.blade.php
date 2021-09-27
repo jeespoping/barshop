add1076dcdeaecb66537637b01aea64dd2bba2bb
jeespoping <jeespoping@gmail.com>
2021-09-27 14:27:56 -0500

    navbar

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- foundation -->
+    <!-- Compressed CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+    <!-- Compressed JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+
+    <link href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css" rel="stylesheet">
+
+    <title>@yield('meta-title', config('app.name') . " | Bar Shop")</title>
+    <meta name="description" content="@yield('meta-description', 'Bar Shop')">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+
+<div id="app">
+    @include('partials.nav')
+
+    <main>
+        @yield('content')
+    </main>
+
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<div class="top-bar">
+    <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+            <li class="menu-text">Bar Shop <i class="far fa-glass-cheers"></i></li>
+            <li><a href="#">Tienda</a></li>
+            @auth
+                <li><a href="#">Admin</a></li>
+            @endauth
+        </ul>
+    </div>
+</div>
